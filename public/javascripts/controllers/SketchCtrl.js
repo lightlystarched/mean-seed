324a6f5a3db595ac8ed87ffc6e1b13808af5335a
Josh Martin <jmartin@outbrain.com>
2014-11-23 15:20:59 -0500

    converted to a d3-only setup version

--- public/javascripts/controllers/SketchCtrl.js
+++
@@ -1,7 +0,0 @@
-define(['angular'], function(angular) {
-	'use strict';
-
-	basApp.controller('SketchCtrl', ['$log', '$scope', function($log, $scope) {
-			$log.debug('Sketch controller loaded');
-		}]);
-});
